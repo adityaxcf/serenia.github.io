6bfd16d50e5168b798f0cdd85a6cdb3fbd6a600f
AJ <92220714+adityaxcf@users.noreply.github.com>
2025-12-09 13:59:15 +0530

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-document.querySelectorAll(".algo-btn").forEach(button => {
-    button.addEventListener("click", () => {
-        const box = button.nextElementSibling;
-        box.classList.toggle("open");
-
-        button.textContent = 
-            box.classList.contains("open") ? "Hide Algorithm" : "Show Algorithm";
-    });
-});
